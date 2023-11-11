7720d6a28e25e988f2525b4e155d836288aec6e9
fnandesrafael <fnandesrafael@pm.me>
2023-11-11 11:17:04 -0300

    feat: Navbar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav id="nav">
+      <div id="logo">
+        <a href="#">Alphaflame</a>
+        <div id="logo-revealer"></div>
+      </div>
+    </nav>
+  );
+}
