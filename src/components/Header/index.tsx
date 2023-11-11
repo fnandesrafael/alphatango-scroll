00a3b480c641eecd9904c9b1c8ed3d6b61e6391b
fnandesrafael <fnandesrafael@pm.me>
2023-11-11 11:17:20 -0300

    feat: Header component jsx

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header id="header">
+      <span id="letter">A</span>
+      <span id="letter">L</span>
+      <span id="letter">P</span>
+      <span id="letter">H</span>
+      <span id="letter">A</span>
+      <span id="letter">F</span>
+      <span id="letter">L</span>
+      <span id="letter">A</span>
+      <span id="letter">M</span>
+      <span id="letter">E</span>
+    </header>
+  );
+}
